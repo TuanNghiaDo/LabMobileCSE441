ffd7266e73802efa6b1a2dba9387d6417382698d
TuanNghiaDo <dotuannghia07@gmail.com>
2025-05-02 21:15:07 +0700

    Handle Exception

--- MyApplication/app/src/main/java/com/examples_1/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/examples_1/myapplication/MainActivity.java
@@ -1,9 +1,11 @@
 package com.examples_1.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,10 +30,25 @@ public class MainActivity extends AppCompatActivity {
         btnSum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int a = Integer.parseInt(edt_A.getText().toString());
-                int b = Integer.parseInt(edt_B.getText().toString());
-                int c = a + b;
-                edtResult.setText(String.valueOf(c));
+                String textA = edt_A.getText().toString().trim();
+                String textB = edt_B.getText().toString().trim();
+                if(textA.isEmpty() || textB.isEmpty()){
+                    // Parameters in the Toast.makeText() method: which activity display?, message,display time
+                    Toast.makeText(MainActivity.this, "Vui lòng nhập đủ cả hai số a và b", Toast.LENGTH_SHORT).show();
+                    edtResult.setText("");
+                    return;
+                }
+                try {
+                    int a = Integer.parseInt(edt_A.getText().toString());
+                    int b = Integer.parseInt(edt_B.getText().toString());
+                    int c = a + b;
+                    edtResult.setText(String.valueOf(c));
+                } catch (NumberFormatException e) {
+                    Toast.makeText(MainActivity.this, "Vui lòng nhập số nguyên!", Toast.LENGTH_SHORT).show();
+                    edtResult.setText("");
+                    Log.e("MainActivity", "Lỗi định dạng số: " + e.getMessage());
+                }
+
             }
         });
     }
