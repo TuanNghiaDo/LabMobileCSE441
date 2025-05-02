5225fc5a0b8f2b491c673293d8f4328a4d3bca1b
TuanNghiaDo <dotuannghia07@gmail.com>
2025-05-02 20:48:04 +0700

    Set OnClick event for btnSum

--- MyApplication/app/src/main/java/com/examples_1/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/examples_1/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.examples_1.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    //declare UI variables
+    EditText edt_A, edt_B, edtResult;
+    Button btnSum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        //Map Ids to interface variables
+        edt_A = findViewById(R.id.edt_A);
+        edt_B = findViewById(R.id.edt_B);
+        edtResult = findViewById(R.id.result);
+        btnSum = findViewById(R.id.sum);
+        //Handle user interactions
+        btnSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(edt_A.getText().toString());
+                int b = Integer.parseInt(edt_B.getText().toString());
+                int c = a + b;
+                edtResult.setText(String.valueOf(c));
+            }
         });
     }
 }
