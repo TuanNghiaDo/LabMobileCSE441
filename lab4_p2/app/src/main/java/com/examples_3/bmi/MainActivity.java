624080a2617b7bd7215415841b3a1a98d454865c
TuanNghiaDo <dotuannghia07@gmail.com>
2025-05-06 16:56:08 +0700

    Set onclick BMI

--- lab4_p2/app/src/main/java/com/examples_3/bmi/MainActivity.java
+++ lab4_p2/app/src/main/java/com/examples_3/bmi/MainActivity.java
@@ -1,6 +1,9 @@
 package com.examples_3.bmi;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,55 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnBMI;
+    EditText edtName, edtHeight, edtWeight, edtBMI, edtDiagnosis;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnBMI = findViewById(R.id.btnBMI);
+        edtName = findViewById(R.id.name);
+        edtHeight = findViewById(R.id.height);
+        edtWeight = findViewById(R.id.weight);
+        edtBMI = findViewById(R.id.BMI);
+        edtDiagnosis = findViewById(R.id.diagnosis);
+
+        btnBMI.setOnClickListener(v -> {
+            String name = edtName.getText().toString();
+            String heightStr = edtHeight.getText().toString();
+            String weightStr = edtWeight.getText().toString();
+
+            if (name.isEmpty() || heightStr.isEmpty() || weightStr.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Vui lòng nhập đủ thông tin", Toast.LENGTH_SHORT).show();
+                return;
+            }else{
+                try {
+                    double height = Double.parseDouble(heightStr) / 100; // Convert cm to m
+                    double weight = Double.parseDouble(weightStr);
+                    double bmi = weight / (height * height);
+                    String diagnosis;
+
+                    if (bmi < 18) {
+                        diagnosis = "Người gầy";
+                    } else if (bmi <= 24.9) {
+                        diagnosis = "Người bình thường";
+                    } else if (bmi <= 29.9) {
+                        diagnosis = "Người béo phì độ I";
+                    } else if(bmi <= 34.9){
+                        diagnosis = "Người béo phì độ II";
+                    }else{
+                        diagnosis = "Người béo phì độ III";
+                    }
+                    edtBMI.setText(String.format("%.2f", bmi));
+                    edtDiagnosis.setText(diagnosis);
+                    return;
+                }catch (NumberFormatException e) {
+                    Toast.makeText(MainActivity.this, "Vui lòng nhập chiều cao và cân nặng hợp lệ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
         });
     }
 }
