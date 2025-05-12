51bbb228a7964928c398c7eecc43a94a481827a7
TuanNghiaDo <dotuannghia07@gmail.com>
2025-05-12 09:02:00 +0700

    Finish Onclick Event

--- lab7/app/src/main/java/com/examples_7/learnintent/MainActivity.java
+++ lab7/app/src/main/java/com/examples_7/learnintent/MainActivity.java
@@ -4,20 +4,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-Button btnMain;
+Button btnResult;
+EditText edtA, edtB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnMain = findViewById(R.id.button_main);
-        btnMain.setOnClickListener(new View.OnClickListener() {
+        btnResult = findViewById(R.id.button_result);
+        edtA = findViewById(R.id.edt_A);
+        edtB = findViewById(R.id.edt_B);
+        btnResult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent1 = new Intent(MainActivity.this, ResultActivity.class);
+                //Pass data from MainActivity to ResultActivity
+                Bundle bundle = new Bundle();
+                int a = Integer.parseInt(edtA.getText().toString());
+                int b = Integer.parseInt(edtB.getText().toString());
+                bundle.putInt("a", a);
+                bundle.putInt("b", b);
+                intent1.putExtras(bundle);
+                //Or use intent1.putExtra("myBundle", bundle);
                 //Display the child activity
                 startActivity(intent1);
             }
--- lab7/app/src/main/java/com/examples_7/learnintent/ResultActivity.java
+++ lab7/app/src/main/java/com/examples_7/learnintent/ResultActivity.java
@@ -4,24 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ResultActivity extends AppCompatActivity {
-    Button btnChild;
+import java.text.DecimalFormat;
 
+public class ResultActivity extends AppCompatActivity {
+    Button btnCancel;
+    TextView tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_result);
-        btnChild = findViewById(R.id.button_child);
-        btnChild.setOnClickListener(new View.OnClickListener() {
+        btnCancel = findViewById(R.id.button_cancel);
+        tvResult = findViewById(R.id.tv_result);
+        Bundle extras = getIntent().getExtras();
+        int a = extras.getInt("a");
+        int b = extras.getInt("b");
+        if(a == 0 && b == 0) {
+            tvResult.setText("There are countless solutions");
+        }else if(a==0 && b!=0) {
+            tvResult.setText("There are no solutions");
+        }else{
+            DecimalFormat dcf = new DecimalFormat("0.##");
+            tvResult.setText(dcf.format(-b*1.0/a));
+        }
+        btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent2 = new Intent(ResultActivity.this, MainActivity.class);
-                //Display the main activity
-                startActivity(intent2);
+                finish();
             }
         });
     }
