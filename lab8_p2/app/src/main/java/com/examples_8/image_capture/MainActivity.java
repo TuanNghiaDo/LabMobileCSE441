06bb23d603589ac33cb4861147d6c0f2619eaf62
TuanNghiaDo <dotuannghia07@gmail.com>
2025-05-12 16:07:43 +0700

    Set ImageButton Camera onclick event

--- lab8_p2/app/src/main/java/com/examples_8/image_capture/MainActivity.java
+++ lab8_p2/app/src/main/java/com/examples_8/image_capture/MainActivity.java
@@ -1,24 +1,49 @@
 package com.examples_8.image_capture;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
+import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
+import android.Manifest;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+    ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        imageView = findViewById(R.id.image_view);
+        imageButton = findViewById(R.id.button_camera);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(ACTION_IMAGE_CAPTURE);
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
+                    return;
+                }
+                startActivityForResult(cameraIntent, 99);
+            }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 99 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
