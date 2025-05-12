ce667f3368901d6c8acf6b165fe789cc12e40303
TuanNghiaDo <dotuannghia07@gmail.com>
2025-05-12 07:29:34 +0700

    Finish Intent1, 2

--- lab7/app/src/main/java/com/examples_7/learnintent/ChildActivity.java
+++ lab7/app/src/main/java/com/examples_7/learnintent/ChildActivity.java
@@ -1,6 +1,9 @@
 package com.examples_7.learnintent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ChildActivity extends AppCompatActivity {
+    Button btnChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_child);
-
+        btnChild = findViewById(R.id.button_child);
+        btnChild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(ChildActivity.this, MainActivity.class);
+                //Display the main activity
+                startActivity(intent2);
+            }
+        });
     }
 }
--- lab7/app/src/main/java/com/examples_7/learnintent/MainActivity.java
+++ lab7/app/src/main/java/com/examples_7/learnintent/MainActivity.java
@@ -1,6 +1,9 @@
 package com.examples_7.learnintent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btnMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btnMain = findViewById(R.id.button_main);
+        btnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, ChildActivity.class);
+                //Display the child activity
+                startActivity(intent1);
+            }
+        });
     }
 }
