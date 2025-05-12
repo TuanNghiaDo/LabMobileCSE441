16254d7f52e39fa259b4ab90c377ea477cdf155d
TuanNghiaDo <dotuannghia07@gmail.com>
2025-05-12 11:02:51 +0700

    Finish Design UI lab8

---
+++ lab8/app/src/main/java/com/examples_8/intent_call_sms/CallPhoneActivity.java
@@ -0,0 +1,20 @@
+package com.examples_8.intent_call_sms;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CallPhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_call_phone);
+
+    }
+}
