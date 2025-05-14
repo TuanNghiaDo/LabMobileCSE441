ef2b8a41df522a69207a265bf150efe0ff868995
TuanNghiaDo <dotuannghia07@gmail.com>
2025-05-14 10:02:35 +0700

    Finish App1 lab11

--- lab11/app/src/main/java/com/examples_11/intent_filter/MainActivity.java
+++ lab11/app/src/main/java/com/examples_11/intent_filter/MainActivity.java
@@ -1,24 +1,30 @@
 package com.examples_11.intent_filter;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btnShow;
+EditText edtLink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnShow = findViewById(R.id.btn_show_web);
+        edtLink = findViewById(R.id.edt_link);
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://" + edtLink.getText().toString()));
+                startActivity(intent);
+            }
         });
     }
 }
