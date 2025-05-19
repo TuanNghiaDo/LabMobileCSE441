3b6464ec9a56935cff2b29350952fe04b9335f66
TuanNghiaDo <dotuannghia07@gmail.com>
2025-05-19 22:38:36 +0700

    Create Image Object

---
+++ lab13_p3/app/src/main/java/com/example/customlayout/models/Image.java
@@ -0,0 +1,29 @@
+package com.example.customlayout.models;
+
+public class Image {
+    private int img;
+    private String name;
+     public int getImg(){
+         return img;
+     }
+
+     public String getName(){
+         return name;
+     }
+
+    public Image(int img, String name) {
+        this.img = img;
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public Image() {
+    }
+}
