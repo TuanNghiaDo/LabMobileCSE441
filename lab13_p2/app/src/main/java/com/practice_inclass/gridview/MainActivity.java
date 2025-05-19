2f71aac84ec91660044c96c0d2d5f6aab7b98ec1
TuanNghiaDo <dotuannghia07@gmail.com>
2025-05-19 17:19:10 +0700

    Finissh Demo GridView

--- lab13_p2/app/src/main/java/com/practice_inclass/gridview/MainActivity.java
+++ lab13_p2/app/src/main/java/com/practice_inclass/gridview/MainActivity.java
@@ -1,24 +1,28 @@
 package com.practice_inclass.gridview;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    String [] arr = {"Ipad", "Iphone", "Macbook", "Apple Watch", "Airpods", "Iphone 14 Pro Max"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        final TextView selection = (TextView) findViewById(R.id.selection);
+        final GridView gridView = (GridView)findViewById(R.id.grid_view1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arr);
+
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedItem = (String) parent.getItemAtPosition(position);
+            selection.setText(selectedItem);
         });
     }
 }
