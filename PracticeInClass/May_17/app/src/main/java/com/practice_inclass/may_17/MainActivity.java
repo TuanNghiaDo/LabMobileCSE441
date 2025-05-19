52e59ea765d76b3b335ff451bb4f1bea64ed4a5c
TuanNghiaDo <dotuannghia07@gmail.com>
2025-05-19 13:34:23 +0700

    finish onclick btnAddWork

--- PracticeInClass/May_17/app/src/main/java/com/practice_inclass/may_17/MainActivity.java
+++ PracticeInClass/May_17/app/src/main/java/com/practice_inclass/may_17/MainActivity.java
@@ -1,8 +1,16 @@
 package com.practice_inclass.may_17;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,12 +21,46 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_options_menu, menu); // Sử dụng tên file menu của bạn
+        Log.d(TAG, "onCreateOptionsMenu called");
+        return true; // true để hiển thị menu
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId(); // Lấy ID của item được chọn
+        Log.d(TAG, "onOptionsItemSelected called for item ID: " + itemId);
+
+        // Kiểm tra ID và thực hiện hành động tương ứng
+        if (itemId == R.id.action_refresh) {
+            // Xử lý khi nhấn Refresh
+            Toast.makeText(this, "Đang làm mới...", Toast.LENGTH_SHORT).show();
+            // Thêm code làm mới dữ liệu ở đây...
+            return true; // Đã xử lý
+
+        } else if (itemId == R.id.action_settings) {
+            // Xử lý khi nhấn Settings
+            Toast.makeText(this, "Mở màn hình Cài đặt...", Toast.LENGTH_SHORT).show();
+            // Intent intent = new Intent(this, SettingsActivity.class);
+            // startActivity(intent);
+            return true; // Đã xử lý
+
+        } else if (itemId == R.id.action_help) {
+            // Xử lý khi nhấn Help
+            Toast.makeText(this, "Mở màn hình Trợ giúp...", Toast.LENGTH_SHORT).show();
+            // Thêm code hiển thị trợ giúp ở đây...
+            return true; // Đã xử lý
+
+        } else {
+            // Nếu không phải các item trên, để lớp cha xử lý (quan trọng cho nút Up/Home)
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
--- lab12_p2/app/src/main/java/com/practice_inclass/calendarnote/MainActivity.java
+++ lab12_p2/app/src/main/java/com/practice_inclass/calendarnote/MainActivity.java
@@ -1,24 +1,79 @@
 package com.practice_inclass.calendarnote;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lv1;
+    ArrayList<String> arrayWork;
+    ArrayAdapter<String> arrAdater;
+    EditText edtWork, edtHour, edtMinute;
+    TextView txtDate;
+    Button btnAddWork;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        lv1 = findViewById(R.id.lv1);
+        edtWork = findViewById(R.id.edt_work);
+        edtHour = findViewById(R.id.edt_hour);
+        edtMinute = findViewById(R.id.edt_minute);
+        txtDate = findViewById(R.id.tv_date);
+        btnAddWork = findViewById(R.id.btn_add_work);
+
+        arrayWork = new ArrayList<>();
+        //Khai bao adapter, dua mang du lieu vao adpater
+        arrAdater = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayWork);
+        //Dua adapter co du lieu len listview
+        lv1.setAdapter(arrAdater);
+        //Lay ngay gio he thong
+        Date currentDate = Calendar.getInstance().getTime();
+        //Format theo dinh dang dd/MM/yyyy
+        java.text.SimpleDateFormat simpleDateFormat = new java.text.SimpleDateFormat("dd/MM/yyyy");
+        txtDate.setText("Hon nay: " + simpleDateFormat.format(currentDate));
+
+        //Su kien click nut them cong viec
+        btnAddWork.setOnClickListener(new View.OnClickListener() { //onClickListener là một interface
+            @Override
+            public void onClick(View v) {
+                //Neu 1 trong 3 edt khong co noi dung thi hien len thong bao bang hop thoai Dialog
+                if (edtWork.getText().toString().equals("") || edtHour.getText().toString().equals("") || edtMinute.getText().toString().equals("")) {
+                    android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Infor missing");
+                    builder.setMessage("Please enter all information of the work");
+                    builder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+                    builder.show();
+                } else {
+                    //Lấy nội dung công việc và thời gian từ EditText và đưa vào mảng Arraywork, cập nhật lại Adapter
+                    String str = edtWork.getText().toString() + " - " + edtHour.getText().toString() + ":" + edtMinute.getText().toString();
+                    //Để thêm dữ liệu vào trong listview chúng ta cần hai bước:
+                    //Bước 1: thêm dữ liệu vào trong mảng
+                    arrayWork.add(str);
+                    //Bước 2: cập nhật lại adapter
+                    arrAdater.notifyDataSetChanged();
+                    edtWork.setText("");
+                    edtHour.setText("");
+                    edtMinute.setText("");
+                }
+            }
         });
     }
 }
