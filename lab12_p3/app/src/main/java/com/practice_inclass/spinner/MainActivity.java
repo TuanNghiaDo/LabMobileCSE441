b906e702518ce3f0fa81c74947102e5e16b9273a
TuanNghiaDo <dotuannghia07@gmail.com>
2025-05-19 14:27:43 +0700

    Init lab12_p3

---
+++ lab12_p3/app/src/main/java/com/practice_inclass/spinner/MainActivity.java
@@ -0,0 +1,16 @@
+package com.practice_inclass.spinner;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
