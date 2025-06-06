685f236917bee091422cf36b64c2ac993112f26f
TuanNghiaDo <dotuannghia07@gmail.com>
2025-06-06 08:17:13 +0700

    Finish lab20

--- lab20/app/src/main/java/com/cse441/arsyntask/MainActivity.java
+++ lab20/app/src/main/java/com/cse441/arsyntask/MainActivity.java
@@ -1,24 +1,36 @@
 package com.cse441.arsyntask;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.cse441.arsyntask.R;
+import com.cse441.arsyntask.model.MyAsyncTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart;
+    MyAsyncTask mytt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnStart = (Button) findViewById(R.id.button1);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                doStart();
+            }
         });
+    }
+
+    private void doStart() {
+        mytt = new MyAsyncTask(this);
+        mytt.execute();
     }
 }
