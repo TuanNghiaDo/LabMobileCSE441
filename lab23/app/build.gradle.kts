24f1a3b221e1c96a95e615a99109ee16bc0465e9
TuanNghiaDo <dotuannghia07@gmail.com>
2025-06-06 09:19:31 +0700

    Finish lab23

--- lab23/app/build.gradle.kts
+++ lab23/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 android {
+    useLibrary("org.apache.http.legacy")
     namespace = "com.cse441.vnexpress"
     compileSdk = 35
 
@@ -37,6 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.crashlytics.buildtools)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ lab23/app/src/main/java/com/cse441/vnexpress/XMLParser.java
@@ -0,0 +1,35 @@
+package com.cse441.vnexpress;
+
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.util.EntityUtils;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class XMLParser {
+
+    public String getXmlFromUrl(String url) {
+        String xml = null;
+
+        try {
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            xml = EntityUtils.toString((com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.HttpEntity) httpEntity);
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return xml;
+    }
+}
