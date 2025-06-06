8f1522ada8997e6ca80f293b2c02198ecf5f5fce
TuanNghiaDo <dotuannghia07@gmail.com>
2025-06-06 09:06:01 +0700

    Finish create class Lisst

---
+++ lab23/app/src/main/java/com/cse441/vnexpress/List.java
@@ -0,0 +1,47 @@
+package com.cse441.vnexpress;
+import android.graphics.Bitmap;
+public class List {
+    private Bitmap img;
+    private String title, info, link;
+
+    //Có thể xây dựng Constructer để thêm dữ liệu cho nhanh
+    public List(Bitmap img, String title, String info, String link) {
+        this.img = img;
+        this.title = title;
+        this.info = info;
+        this.link = link;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    // Tên phương thức được giữ nguyên như trong hình (chữ 'l' viết thường)
+    public String getlink() {
+        return link;
+    }
+
+    public void setlink(String link) {
+        this.link = link;
+    }
+
+    public Bitmap getImg() {
+        return img;
+    }
+
+    public void setImg(Bitmap img) {
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ lab23/app/src/main/java/com/cse441/vnexpress/MyArrayAdapter.java
@@ -0,0 +1,4 @@
+package com.cse441.vnexpress;
+
+public class MyArrayAdapter {
+}
