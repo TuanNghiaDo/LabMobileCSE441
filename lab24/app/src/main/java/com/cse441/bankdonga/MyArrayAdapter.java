7c0cbfac448ef0a3fe1c7da753a0681690348f6d
TuanNghiaDo <dotuannghia07@gmail.com>
2025-06-11 09:08:26 +0700

    Finish MyArrayAdapter

--- lab24/app/src/main/java/com/cse441/bankdonga/MyArrayAdapter.java
+++ lab24/app/src/main/java/com/cse441/bankdonga/MyArrayAdapter.java
@@ -1,4 +1,54 @@
 package com.cse441.bankdonga;
 
-public class MyArrayAdapter {
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+// Giả sử bạn đã tạo file R.java và Tygia.java
+// import com.yourpackage.R;
+// import com.yourpackage.Tygia;
+
+public class MyArrayAdapter extends ArrayAdapter<Tygia> {
+
+    Activity context;
+    int resource;
+    List<Tygia> objects;
+
+    public MyArrayAdapter(Activity context, int resource, List<Tygia> objects) {
+        super(context, resource, objects);
+        // TODO Auto-generated constructor stub
+        this.context = context;
+        this.objects = objects;
+        this.resource = resource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        LayoutInflater inflater = this.context.getLayoutInflater();
+        View item = inflater.inflate(this.resource, null);
+
+        Tygia tygia = this.objects.get(position);
+        ImageView imgHinh = (ImageView) item.findViewById(R.id.imgHinh);
+        TextView txtType = (TextView) item.findViewById(R.id.txtType);
+        TextView txtMuaTM = (TextView) item.findViewById(R.id.txtMuaTM);
+        TextView txtBanTM = (TextView) item.findViewById(R.id.txtBanTM);
+        TextView txtMuaCK = (TextView) item.findViewById(R.id.txtMuaCK);
+        TextView txtBanCK = (TextView) item.findViewById(R.id.txtBanCK);
+
+        imgHinh.setImageBitmap(tygia.getBitmap());
+        txtType.setText(tygia.getType());
+        txtMuaTM.setText(tygia.getMuatienmat());
+        txtBanTM.setText(tygia.getBantienmat()); // Lưu ý: Tên phương thức này có thể bị gõ nhầm
+        txtMuaCK.setText(tygia.getMuack());
+        txtBanCK.setText(tygia.getBanck());
+
+        return item;
+    }
 }
